2d149eeb30db35e9fdc3b25ac614cac843137b94
Anton Kononov <sm1le88man@gmail.com>
2023-03-12 12:05:39 +0300

    update greetings in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Welcome to the Brain Games!");
+        Thread.sleep(1000);
+        System.out.println("May I have your name?");
+        String name = s.next().toLowerCase();
+        name = name.substring(0, 1).toUpperCase() + name.substring(1);
+        System.out.println();
+        System.out.println("Hello, " + name + "!");
+        s.close();
     }
+
 }
