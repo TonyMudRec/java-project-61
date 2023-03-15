73951b65fce37c6253a59e6663950739e1598220
TonyMudRec <98638334+TonyMudRec@users.noreply.github.com>
2023-03-15 14:42:54 +0300

    Update and rename Game.java to Engine.java

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,37 @@
+package hexlet.code;
+
+public interface Engine {
+    String getDescription();
+
+    String game();
+
+    default void process() {
+        int gameCount = 0;
+        boolean winGame = false;
+        boolean gameMusGoOn = true;
+        String correctAnswer = null;
+        String answer = null;
+
+        Cli.greetings();
+        System.out.println(getDescription());
+        while (gameMusGoOn) {
+            correctAnswer = game();
+            answer = App.s.next();
+            gameMusGoOn = answer.equals(correctAnswer);
+            if (gameMusGoOn && gameCount < 2) {
+                gameCount++;
+                System.out.println("Correct!");
+            } else {
+                winGame = gameCount == 2;
+                gameMusGoOn = false;
+            }
+        }
+        System.out.println(endGame(Cli.name, answer, winGame, correctAnswer));
+    }
+
+    public default String endGame(String name, String answer, boolean winGame, String correctAnswer) {
+        return winGame ? "Congratulations, " + name + "!" : "'" + answer + "' is wrong answer ;(. Correct answer was '"
+                + correctAnswer + "'." + "\n" + "Let's try again, " + name + "!";
+    }
+}
+
