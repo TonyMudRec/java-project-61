34452e7f0172ef76e4c444ab5d2db4dcd316effd
TonyMudRec <98638334+TonyMudRec@users.noreply.github.com>
2023-03-15 17:51:56 +0300

    Add Calc.java

---
+++ app/src/main/java/hexlet/code/Calc.java
@@ -0,0 +1,28 @@
+public class Calc implements Engine {
+    @Override
+    public String getDescription() {
+        return "What is the result of the expression?";
+    }
+
+    @Override
+    public String game() {
+        int randomValue1 = (int) (Math.random() * 100);
+        int randomValue2 = (int) (Math.random() * 10);
+        int randomExpression = 0;
+        String expression = null;
+
+        if (Math.random() < 0.3) {
+            randomExpression = randomValue1 + randomValue2;
+            expression = "+";
+        } else if (Math.random() >= 0.3 && Math.random() < 0.6) {
+            randomExpression = randomValue1 - randomValue2;
+            expression = "-";
+        } else {
+            randomExpression = randomValue1 * randomValue2;
+            expression = "*";
+        }
+        System.out.println("Question: " + randomValue1 + " " + expression + " " + randomValue2);
+        System.out.print("Your answer: ");
+        return Integer.toString(randomExpression);
+    }
+}
