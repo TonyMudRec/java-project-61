593914ca5ccdd3fe6290648c71f4745b801227e5
TonyMudRec <98638334+TonyMudRec@users.noreply.github.com>
2023-03-16 10:03:41 +0300

    Add gcd to App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,7 +11,8 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
-        System.out.println("4 - Exit");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Exit");
         System.out.print("Your choice: ");
         int gameChoice = s.nextInt();
 
@@ -24,6 +25,10 @@ public class App {
             case 3 -> {
                 Calc calc = new Calc();
                 calc.process();
+            }
+            case 4 -> {
+                GCD gcd = new GCD();
+                gcd.process();
             }
             default -> {
             }
